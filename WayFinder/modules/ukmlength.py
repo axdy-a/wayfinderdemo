47c6c78f4515e90e0e4bd3db12a480b737bb175c
ftt.jun.ang <ftt.jun.ang@grabtaxi.com>
2025-01-10 15:31:36 +0800

    Minor fixes

--- WayFinder/modules/ukmlength.py
+++ WayFinder/modules/ukmlength.py
@@ -7,7 +7,6 @@ def getLength(wayids):
         waylist = str(wayids).strip()
         data = api.overpass_query(api.ukm_length_query(waylist)) 
         return f"{float(data["elements"][-1]["tags"]["length"])/1000:.4f}"
-    
     except:
         return "Error: Check your way id input"
 
